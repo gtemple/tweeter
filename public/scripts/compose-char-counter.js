eed6a10cb83698eab509dd8a797f8892fa576e39
Giordano Temple <giordanotemple@gmail.com>
2023-01-31 20:13:35 +0000

    added tweet stylings

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -2,13 +2,13 @@ $( document ).ready(function() {
   console.log( "ready!" );
   let tweetLength;
 
-  $( '#tweet-text' ).keyup(function(){
+  $( '.tweet-text' ).keyup(function(){
     tweetLength = 140 - $(this).val().length; 
     $(this).next(".tweet-form-bottom").children('#tweet-counter').html(tweetLength);
 
     tweetLength < 0 ?
     $(this).next(".tweet-form-bottom").children("#tweet-counter").addClass("past-limit") :
-    $(this).next(".tweet-form-bottom").children("#tweet-counter").removeClass("past-limit")
+    $(this).next(".tweet-form-bottom").children("#tweet-counter").removeClass("past-limit");
   });
 });
 
